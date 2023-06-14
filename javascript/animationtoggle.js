a1b3de138df7e49dc373f37586231f3c2d076e8c
Namanqw1 <nasuchacha1708@gmail.com>
2023-06-14 09:12:24 +0530

    couser dropdown animation completed

--- javascript/animationtoggle.js
+++ javascript/animationtoggle.js
@@ -6,10 +6,18 @@ function toggleAnimation(elementId) {
   if (animationState === "exit-animation") {
     myDiv.classList.remove("exit-animation");
     myDiv.classList.add("enter-animation");
+    setTimeout(() => {
+      myDiv.style.height = "169px";
+    }, 299);
+    
     animationState = "enter-animation";
   } else {
     myDiv.classList.remove("enter-animation");
     myDiv.classList.add("exit-animation");
+    setTimeout(() => {
+      myDiv.style.height = "0px";
+      myDiv.style.display = "none";
+    }, 299);
     animationState = "exit-animation";
   }
 }
